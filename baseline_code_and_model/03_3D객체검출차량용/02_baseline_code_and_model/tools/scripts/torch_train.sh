3cebaf0186f4364617b4caeb37bcddef1ed8156d
YoungkiKim <gkdlgkdl1231@gmail.com>
2024-10-20 19:53:10 +0900

    revise shell scipts(torch_train, torch_val) DSVT Pillar train complete

--- baseline_code_and_model/03_3D객체검출차량용/02_baseline_code_and_model/tools/scripts/torch_train.sh
+++ baseline_code_and_model/03_3D객체검출차량용/02_baseline_code_and_model/tools/scripts/torch_train.sh
@@ -1,8 +1,34 @@
 #!/usr/bin/env bash
 
 set -x
-NGPUS=$1
 PY_ARGS=${@:2}
+
+# NUM_GPU
+NGPUS="2"
+
+
+CFG="--cfg_file "
+# write configuration file path
+CFG_FILE="cfgs/waymo_models/dsvt_voxel_whole_range6.yaml"
+CFG_ARG="$CFG $CFG_FILE"
+
+
+CKPT="--ckpt"
+# if you want to use ckpt.pth, 
+# change CKPT_TRUE = true
+CKPT_USE=false
+# write ckpt .pth file_path 
+CKPT_PATH="/home/ailab/git/Team_3/baseline_code_and_model/03_3D객체검출차량용/02_baseline_code_and_model/output/waymo_models/dsvt_pillar_whole/default/ckpt/checkpoint_epoch_20.pth"
+CKPT_ARG="$CKPT $CKPT_PATH"
+
+
+# whole arguments
+if ["$USE_CKPT" = "true"]; then
+    ARG="$CFG_ARG $CKPT_ARG"
+else
+    ARG="$CFG_ARG"
+fi
+
 
 while true
 do
@@ -14,7 +40,7 @@ do
 done
 echo $PORT
 
-CUDA_VISIBLE_DEVICES=2,3 torchrun --nproc_per_node=${NGPUS} --rdzv_endpoint=localhost:${PORT} train.py --launcher pytorch ${PY_ARGS}
+CUDA_VISIBLE_DEVICES=2,3 torchrun --nproc_per_node=${NGPUS} --rdzv_endpoint=localhost:${PORT} train.py --launcher pytorch ${ARG}
 # 나머지
 # CUDA_VISIBLE_DEVICES=2 torchrun --nproc_per_node=${NGPUS} --rdzv_endpoint=localhost:${PORT} train.py --launcher pytorch ${PY_ARGS}
 
--- baseline_code_and_model/03_3D객체검출차량용/02_baseline_code_and_model/tools/scripts/torch_val.sh
+++ baseline_code_and_model/03_3D객체검출차량용/02_baseline_code_and_model/tools/scripts/torch_val.sh
@@ -1,8 +1,24 @@
 #!/usr/bin/env bash
 
 set -x
-NGPUS=$1
 PY_ARGS=${@:2}
+
+
+NGPUS="2"
+
+CFG="--cfg_file "
+# write configuration file path
+CFG_FILE="cfgs/waymo_models/dsvt_pillar_whole.yaml"
+CFG_ARG="$CFG $CFG_FILE"
+
+CKPT="--ckpt"
+# write ckpt .pth file_path
+CKPT_PATH="/home/ailab/git/Team_3/baseline_code_and_model/03_3D객체검출차량용/02_baseline_code_and_model/output/waymo_models/dsvt_pillar_whole/default/ckpt/checkpoint_epoch_20.pth"
+CKPT_ARG="$CKPT $CKPT_PATH"
+
+# whole arguments
+ARG="$CFG_ARG $CKPT_ARG"
+
 
 while true
 do
@@ -14,4 +30,5 @@ do
 done
 echo $PORT
 
-CUDA_VISIBLE_DEVICES=2,3 torchrun --nproc_per_node=${NGPUS} --rdzv_endpoint=localhost:${PORT} val.py --launcher pytorch ${PY_ARGS}
+
+CUDA_VISIBLE_DEVICES=2,3 torchrun --nproc_per_node=${NGPUS} --rdzv_endpoint=localhost:${PORT} val.py --launcher pytorch ${ARG}
